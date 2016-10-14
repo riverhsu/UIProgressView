f5bf9bd50201ac364f26e98d47099752a358d6b9
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-14 10:29:52 +0800

    This is a basic function of UIProgressView.  It would know the attributes of progress view and how to control it. Test is OK

--- UIProgressView/ViewController.swift
+++ UIProgressView/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressView: UIProgressView!
+    var timeTapped : Float = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.timeTapped = 0
+        self.progressView.progress = 0
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        self.timeTapped += 0.1
+        
+        if self.timeTapped == 1.0 {
+            self.progressView.progress = 1.0
+            return
+        }
+        
+        self.progressView.progress = self.timeTapped
+    }
 
 }
 
