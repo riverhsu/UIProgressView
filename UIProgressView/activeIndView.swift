70ce70ecef95cc73c8a5d87c1f58e149a763c238
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-14 11:18:21 +0800

    Initiate Activity Indicator View

---
+++ UIProgressView/activeIndView.swift
@@ -0,0 +1,27 @@
+//
+//  activeIndView.swift
+//  UIProgressView
+//
+//  Created by Sgmedical on 2016/10/14.
+//  Copyright © 2016年 Sgmedical. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class activeIndViewController: UIViewController{
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+    }
+    
+    @IBAction func runActivityIndicator(_ sender: UIButton) {
+        
+    }
+}
